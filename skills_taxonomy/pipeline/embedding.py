7af3cb7b71eebc1ae4a265aefb90362dcbacc0d0
Jack Rasala <47244945+Jack-0-0@users.noreply.github.com>
2021-05-11 20:42:49 +0000

    add embedding to pipeline (#11)

---
+++ skills_taxonomy/pipeline/embedding.py
@@ -0,0 +1,18 @@
+# File: pipeline/embedding.py
+"""Create embedding using skills descriptions
+"""
+
+from sentence_transformers import SentenceTransformer
+from skills_taxonomy import config
+
+
+def create_emebedding(skills):
+    """Create embedding using sentence transformer
+
+    Returns:
+        embedding: skill descriptions embedding
+    """
+    corpus = list(skills["description"].values)
+    embedder = SentenceTransformer(config["sentence_transformer"]["model"])
+    embedding = embedder.encode(corpus, show_progress_bar=True)
+    return embedding
