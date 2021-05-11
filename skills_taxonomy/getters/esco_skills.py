4f12c2d929376e17d638d367d554665f0c99a750
Jack Rasala <47244945+Jack-0-0@users.noreply.github.com>
2021-05-11 13:49:13 +0000

    add skills data getter (#5)

---
+++ skills_taxonomy/getters/esco_skills.py
@@ -0,0 +1,16 @@
+# File: getters/esco_skills.py
+"""Data getters for the esco skills data.
+
+Data source: https://ec.europa.eu/esco/portal
+"""
+import pandas as pd
+from skills_taxonomy import PROJECT_DIR
+
+
+def get_skills() -> pd.DataFrame:
+    """Load esco skills.
+
+    Returns:
+        dataframe of esco skills
+    """
+    return pd.read_csv(f"{PROJECT_DIR}/inputs/data/skills_en.csv")
