c44a5f1220d3d2ad9175839835771c06db88d4b1
Jack Rasala <47244945+Jack-0-0@users.noreply.github.com>
2021-05-12 16:44:35 +0000

    12 save load embedding (#13)
    * fixed create_embedding typo
    
    * add load and save functions

--- skills_taxonomy/pipeline/embedding.py
+++ skills_taxonomy/pipeline/embedding.py
@@ -1,12 +1,17 @@
 # File: pipeline/embedding.py
-"""Create embedding using skills descriptions
+"""Functions to create, load and save skills embedding
 """
 
 from sentence_transformers import SentenceTransformer
 from skills_taxonomy import config
+import pickle
+import logging
+from skills_taxonomy import PROJECT_DIR
+
+logger = logging.getLogger(__name__)
 
 
-def create_emebedding(skills):
+def create_embedding(skills):
     """Create embedding using sentence transformer
 
     Returns:
@@ -16,3 +21,31 @@ def create_emebedding(skills):
     embedder = SentenceTransformer(config["sentence_transformer"]["model"])
     embedding = embedder.encode(corpus, show_progress_bar=True)
     return embedding
+
+
+def save_embedding(embedding):
+    """Save embedding as pickle file
+
+    Args:
+        embedding: skills embedding to be saved as pickle file
+    """
+    with open(f"{PROJECT_DIR}/outputs/models/embedding.pkl", "wb") as out:
+        pickle.dump(embedding, out)
+
+
+def load_embedding(path=f"{PROJECT_DIR}/outputs/models/embedding.pkl"):
+    """Load embedding from pickle file
+
+    Args:
+        path: path to pickle file to load
+
+    Returns:
+        embedding: skills embedding loaded from pickle file
+    """
+    try:
+        with open(path, "rb") as inp:
+            embedding = pickle.load(inp)
+        return embedding
+
+    except FileNotFoundError:
+        logger.info(f"There is no embedding to load at {path}")
