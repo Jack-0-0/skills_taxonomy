7025eef71bd3558ce5b72a9cf72c53b512ff36af
Jack Rasala <47244945+Jack-0-0@users.noreply.github.com>
2021-07-01 19:09:14 +0200

    31 Refactor clustering and preprocessing (#32)

--- skills_taxonomy/pipeline/run.py
+++ skills_taxonomy/pipeline/run.py
@@ -4,11 +4,7 @@ from skills_taxonomy.pipeline.embedding import (
     save_embedding,
     load_embedding,
 )
-from skills_taxonomy.pipeline.clustering import (
-    normalise_embedding,
-    cluster,
-    full_sub_cluster_assignment,
-)
+from skills_taxonomy.pipeline.clustering import cluster_add_ids
 from skills_taxonomy.pipeline.informative_words import save_informative_words
 from skills_taxonomy.pipeline.name_clusters import save_named_clusters
 from skills_taxonomy.pipeline.add_labels import add_labels
@@ -22,22 +18,8 @@ embedding = create_embedding(skills)
 save_embedding(embedding)
 embedding = load_embedding()
 
-# cluster skills
-embedding = normalise_embedding(embedding)
-clustering_model = cluster(embedding)
-cluster_assignments = clustering_model.labels_
-num_clusters = clustering_model.n_clusters_
-full_sub_cluster_assignments = full_sub_cluster_assignment(
-    embedding, cluster_assignments, num_clusters
-)
-skills = skills.assign(
-    class_id=cluster_assignments, subclass_id=full_sub_cluster_assignments
-)
-
-# import pandas as pd
-# skills = pd.read_csv(
-#     f"{PROJECT_DIR}/outputs/skills/skills_after_ids.csv", index_col=[0]
-# )
+# cluster skills and add cluster and subcluster id cols to skills
+skills = cluster_add_ids(skills, embedding)
 
 # create informative words
 save_informative_words()
@@ -49,4 +31,4 @@ save_named_clusters()
 add_labels(skills)
 
 # save updated skills
-skills.to_csv(f"{PROJECT_DIR}/outputs/skills/skills_after_labels.csv")
+skills.to_csv(f"{PROJECT_DIR}/outputs/skills/skills_after_labels2.csv")
