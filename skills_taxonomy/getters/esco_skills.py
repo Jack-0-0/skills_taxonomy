bf75814affce7134702944eee236228e1026240f
Jack Rasala <jack.rasala@nesta.org.uk>
2021-07-02 16:18:19 +0200

    add get_output_skills data getter

--- skills_taxonomy/getters/esco_skills.py
+++ skills_taxonomy/getters/esco_skills.py
@@ -3,7 +3,7 @@
 Data source: https://ec.europa.eu/esco/portal
 """
 import pandas as pd
-from skills_taxonomy import PROJECT_DIR
+from skills_taxonomy import PROJECT_DIR, logger
 
 
 def get_skills() -> pd.DataFrame:
@@ -13,3 +13,12 @@ def get_skills() -> pd.DataFrame:
         df: dataframe of esco skills
     """
     return pd.read_csv(f"{PROJECT_DIR}/inputs/data/skills_en.csv")
+
+
+def get_output_skills(
+    skills_path=f"{PROJECT_DIR}/outputs/skills/labelled_skills.csv",
+) -> pd.DataFrame:
+    try:
+        return pd.read_csv(skills_path, index_col=[0])
+    except FileNotFoundError:
+        logger.error(f"There is no file to read at {skills_path}")
